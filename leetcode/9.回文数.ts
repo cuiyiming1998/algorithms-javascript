11190e483712acafcca9eb3fc95cab3fe4c93392
cuiyiming1998 <1269567664@qq.com>
2022-07-12 14:39:03 +0800

    feat: 添加leetcode 09

---
+++ leetcode/9.回文数.ts
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=9 lang=typescript
+ *
+ * [9] 回文数
+ */
+
+// @lc code=start
+function isPalindrome(x: number): boolean {
+  if (0 === x) {
+    return true
+  }
+  if (x < 0 || 0 === x % 10) {
+    return false
+  }
+  let reverseX = 0
+  let rest = x
+  while (rest >= 10) {
+    reverseX = (reverseX * 10) + (rest % 10)
+    rest = Math.floor(rest / 10)
+  }
+  return x === (reverseX * 10 + rest)
+};
+// @lc code=end
+
+
