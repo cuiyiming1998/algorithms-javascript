6b32164962f3c7e02c5d2999981880cf32974ee0
cuiyiming1998 <1269567664@qq.com>
2022-07-14 15:00:04 +0800

    feat: 添加leetcode 745

---
+++ leetcode/33.搜索旋转排序数组.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=33 lang=typescript
+ *
+ * [33] 搜索旋转排序数组
+ */
+
+// @lc code=start
+function search(nums: number[], target: number): number {
+
+};
+// @lc code=end
+
---
+++ leetcode/745.前缀和后缀搜索.ts
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=745 lang=typescript
+ *
+ * [745] 前缀和后缀搜索
+ */
+
+// @lc code=start
+class WordFilter {
+  // 思路: 暴力法
+  // 维护一个map, 里面存储所有可能的前缀后缀的组合, 使用特殊字符进行连接, 作为key
+  // 检索时使用相同的[ 前缀+特殊符号+后缀 ]的组合进行map.get
+  private dict = new Map<string, number>()
+  private symbol: string = '#'
+  constructor(words: string[]) {
+    for (let i = 0; i < words.length; i++) {
+      const word = words[i]
+      const len = word.length
+      for (let prefixIndex = 1; prefixIndex <= len; prefixIndex++) {
+        for (let suffixIndex = 1; suffixIndex <= len; suffixIndex++) {
+          let key = `${word.substring(0, prefixIndex)}${
+            this.symbol
+          }${word.substring(len - suffixIndex)}`
+          this.dict.set(key, i)
+        }
+      }
+    }
+  }
+
+  f(pref: string, suff: string): number {
+    return this.dict.get(`${pref}${this.symbol}${suff}`) ?? -1
+  }
+}
+
+/**
+ * Your WordFilter object will be instantiated and called as such:
+ * var obj = new WordFilter(words)
+ * var param_1 = obj.f(pref,suff)
+ */
+// @lc code=end
