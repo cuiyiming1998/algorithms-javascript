07523b2b5f092b7245d1857c013ac5ad0ef15135
cuiyiming1998 <1269567664@qq.com>
2022-07-21 18:16:58 +0800

    feat: leetcode 617

---
+++ leetcode/617.合并二叉树.ts
@@ -0,0 +1,40 @@
+/*
+ * @lc app=leetcode.cn id=617 lang=typescript
+ *
+ * [617] 合并二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// @ts-nocheck
+function mergeTrees(
+  root1: TreeNode | null,
+  root2: TreeNode | null
+): TreeNode | null {
+  // 思路: dfs或bfs
+  if (null === root1) {
+    return root2
+  }
+  if (null === root2) {
+    return root1
+  }
+  return new TreeNode(
+    root1.val + root2.val,
+    mergeTrees(root1.left, root2.left),
+    mergeTrees(root1.right, root2.right)
+  )
+}
+// @lc code=end
