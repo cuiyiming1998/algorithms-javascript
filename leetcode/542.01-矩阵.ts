b6cd20ecabc733af560cf38b500911da12585476
cuiyiming1998 <1269567664@qq.com>
2022-07-29 11:52:37 +0800

    feat: leetcode 593

---
+++ leetcode/542.01-矩阵.ts
@@ -0,0 +1,49 @@
+/*
+ * @lc app=leetcode.cn id=542 lang=typescript
+ *
+ * [542] 01 矩阵
+ */
+
+// @lc code=start
+function updateMatrix(mat: number[][]): number[][] {
+  let map: number[][] = new Array(mat.length)
+  let queue: number[][] = []
+  for (let i = 0; i < mat.length; i++) {
+    map[i] = new Array(mat[0].length).fill(Infinity)
+    for (let j = 0; j < mat[0].length; j++) {
+      if (mat[i][j] == 0) {
+        queue.push([i, j])
+        map[i][j] = 0
+      }
+    }
+  }
+  // 寻找0旁边的1
+  while (queue.length) {
+    let size: number = queue.length
+    for (let k = 0; k < size; k ++) {
+      let arr = queue.shift()
+      if (arr) {
+        let [i, j] = arr
+        if (i - 1 >= 0 && map[i - 1][j] > map[i][j] + 1) {
+          map[i - 1][j] = map[i][j] + 1
+          queue.push([i - 1, j])
+        }
+        if (i + 1 < map.length && map[i + 1][j] > map[i][j] + 1) {
+          map[i + 1][j] = map[i][j] + 1
+          queue.push([i + 1, j])
+        }
+        if (j - 1 >= 0 && map[i][j - 1] > map[i][j] + 1) {
+          map[i][j - 1] = map[i][j] + 1
+          queue.push([i, j - 1])
+        }
+        if (j + 1 < map[0].length && map[i][j + 1] > map[i][j] + 1) {
+          map[i][j + 1] = map[i][j] + 1
+          queue.push([i, j + 1])
+        }
+      }
+    }
+  }
+
+  return map
+}
+// @lc code=end
---
+++ leetcode/593.有效的正方形.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=593 lang=typescript
+ *
+ * [593] 有效的正方形
+ */
+
+// @lc code=start
+function validSquare(p1: number[], p2: number[], p3: number[], p4: number[]): boolean {
+  // 思路: 直接模拟
+  // 从4个点中选3个点 检查是否能构成直角三角形
+  // 保存直角边长 和剩下的作比较 如果都相等 则true
+  return calc(p1, p2, p3) && calc(p1, p2, p4) && calc(p1, p3, p4) && calc(p2, p3, p4)
+};
+
+function calc(a: number[], b: number[], c: number[]): boolean {
+  const l1: number = (a[0] - b[0]) ** 2 + (a[1] - b[1]) ** 2
+  const l2: number = (a[0] - c[0]) ** 2 + (a[1] - c[1]) ** 2
+  const l3: number = (b[0] - c[0]) ** 2 + (b[1] - c[1]) ** 2
+  if (!l1 || !l2 || !l3) {
+    return false
+  }
+  const flag = (l1 == l2 && l1 + l2 == l3) || (l1 == l3 && l1 + l3 == l2) || (l2 == l3 && l2 + l3 == l1)
+  return flag
+}
+// @lc code=end
+
