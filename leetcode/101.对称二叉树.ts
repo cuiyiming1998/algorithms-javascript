8fd0d87deadb89faaf389f8b8310c345dd3caabb
cuiyiming1998 <1269567664@qq.com>
2022-08-04 17:25:13 +0800

    feat: leetcode 101

---
+++ leetcode/101.对称二叉树.ts
@@ -0,0 +1,41 @@
+/*
+ * @lc app=leetcode.cn id=101 lang=typescript
+ *
+ * [101] 对称二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// @ts-nocheck
+function isSymmetric(root: TreeNode | null): boolean {
+  return check(root, root)
+}
+
+const check = (left: TreeNode | null, right: TreeNode | null): boolean => {
+  if (!left && !right) {
+    return true
+  }
+  if (!left || !right) {
+    return false
+  }
+
+  return (
+    left.val === right.val &&
+    check(left.left, right.right) &&
+    check(left.right, right.left)
+  )
+}
+// @lc code=end
