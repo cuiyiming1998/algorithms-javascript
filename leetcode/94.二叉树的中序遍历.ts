3959916063339ac64dd485960811d90d06deb15c
cuiyiming1998 <1269567664@qq.com>
2022-08-04 15:55:49 +0800

    feat: leetcode 94

---
+++ leetcode/94.二叉树的中序遍历.ts
@@ -0,0 +1,39 @@
+/*
+ * @lc app=leetcode.cn id=94 lang=typescript
+ *
+ * [94] 二叉树的中序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// @ts-nocheck
+function inorderTraversal(root: TreeNode | null): number[] {
+  // 思路: 递归
+  // 中序 left -> root -> right
+  let res: number[] = []
+  const inorder = (root) => {
+    if (!root) {
+      return
+    }
+    inorder(root.left)
+    res.push(root.val)
+    inorder(root.right)
+  }
+  inorder(root)
+  return res
+};
+// @lc code=end
+
