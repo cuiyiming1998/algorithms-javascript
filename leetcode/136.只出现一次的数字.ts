69c852c49862798e1d35b026eeddaeee10c54595
cuiyiming1998 <1269567664@qq.com>
2022-08-08 14:52:47 +0800

    feat: leetcode 135

---
+++ leetcode/136.只出现一次的数字.ts
@@ -0,0 +1,20 @@
+/*
+ * @lc app=leetcode.cn id=136 lang=typescript
+ *
+ * [136] 只出现一次的数字
+ */
+
+// @lc code=start
+function singleNumber(nums: number[]): number {
+  // 思路: 异或
+  // 任何数和0异或 结果都等于它本身
+  // 任何数和自身异或 结果都等于0
+  // 异或运算满足交换律和结合律
+  let res: number = 0
+  for (const i of nums) {
+    res ^= i
+  }
+  return res
+};
+// @lc code=end
+
