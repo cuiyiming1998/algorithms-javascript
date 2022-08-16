6490eb59a7f0adbcafb9e05eea187d7c415cca10
cuiyiming1998 <1269567664@qq.com>
2022-08-16 18:50:52 +0800

    feat: leetcode 1656

---
+++ leetcode/1656.设计有序流.ts
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=1656 lang=typescript
+ *
+ * [1656] 设计有序流
+ */
+
+// @lc code=start
+class OrderedStream {
+  // 思路: 模拟
+  // 如果插入位置在ptr 则移动 然后Push
+    private ptr: number
+    private data: string[]
+    constructor(n: number) {
+      this.ptr = 0
+      this.data = new Array<string>(n)
+    }
+
+    insert(idKey: number, value: string): string[] {
+      this.data[--idKey] = value
+      let res: string[] = []
+      while (this.ptr < this.data.length && this.data[this.ptr]) {
+        res.push(this.data[this.ptr ++])
+      }
+      return res
+    }
+}
+
+/**
+ * Your OrderedStream object will be instantiated and called as such:
+ * var obj = new OrderedStream(n)
+ * var param_1 = obj.insert(idKey,value)
+ */
+// @lc code=end
+
---
+++ leetcode/640.求解方程.ts
@@ -0,0 +1,12 @@
+/*
+ * @lc app=leetcode.cn id=640 lang=typescript
+ *
+ * [640] 求解方程
+ */
+
+// @lc code=start
+function solveEquation(equation: string): string {
+
+};
+// @lc code=end
+
