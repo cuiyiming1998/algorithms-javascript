e949143df25c3641d3a7065f01e892c5aceb5540
cuiyiming1998 <1269567664@qq.com>
2022-08-16 19:01:43 +0800

    feat: leetcode 217

---
+++ leetcode/217.存在重复元素.ts
@@ -0,0 +1,25 @@
+/*
+ * @lc app=leetcode.cn id=217 lang=typescript
+ *
+ * [217] 存在重复元素
+ */
+
+// @lc code=start
+function containsDuplicate(nums: number[]): boolean {
+  // 思路:
+  // 1. 使用set 过后如果length小于原数组, 则说明有重复
+  // 2. 使用map保存每一个值 如果遇到map中存在的则有重复
+
+  // return new Set(nums).size < nums.length;
+
+  let map = new Map<number, number>()
+  for (const num of nums) {
+    if (map.has(num)) {
+      return true
+    }
+    map.set(num, num)
+  }
+  return false
+};
+// @lc code=end
+
