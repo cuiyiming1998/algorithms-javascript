b59b4493878b29426219957a6bd53a1c907a677a
cuiyiming1998 <1269567664@qq.com>
2022-08-18 10:55:47 +0800

    feat: leetcode 88 350

---
+++ leetcode/350.两个数组的交集-ii.ts
@@ -0,0 +1,33 @@
+/*
+ * @lc app=leetcode.cn id=350 lang=typescript
+ *
+ * [350] 两个数组的交集 II
+ */
+
+// @lc code=start
+function intersect(nums1: number[], nums2: number[]): number[] {
+  // 思路: map
+  // 使用map存储nums1中值出现的次数
+  // 循环nums2在map中寻找, 如果找到 -> 次数 - 1 push到res中
+  let res: number[] = []
+  let map = new Map<number, number>()
+  // 1. 构建map
+  for (const num of nums1) {
+    let cur: number | undefined = map.get(num)
+    // 如果map中存在, 则值 + 1
+    cur = cur ? cur + 1 : 1
+    map.set(num, cur)
+  }
+  // 循环nums2, 寻找相同的值
+  for (const num of nums2) {
+    let cur: number | undefined = map.get(num)
+    if (cur) {
+      cur --
+      res.push(num)
+      map.set(num, cur)
+    }
+  }
+  return res
+};
+// @lc code=end
+
---
+++ leetcode/88.合并两个有序数组.ts
@@ -0,0 +1,35 @@
+/*
+ * @lc app=leetcode.cn id=88 lang=typescript
+ *
+ * [88] 合并两个有序数组
+ */
+
+// @lc code=start
+/**
+  Do not return anything, modify nums1 in-place instead.
+ */
+function merge(nums1: number[], m: number, nums2: number[], n: number): void {
+  // 思路: 逆向双指针
+  // nums1后半部分是空的
+  // 从nums1, nums2最后一个元素开始互相比较, 大的放入num1的最后面
+  let p1: number = m - 1
+  let p2: number = n - 1
+  let len: number = m + n - 1 // 总长度
+  let cur: number
+
+  while (p1 >= 0 || p2 >= 0) {
+    // 如果一个数组减没了
+    if (-1 === p1) {
+      cur = nums2[p2 --]
+    } else if (-1 === p2) {
+      cur = nums1[p1 --]
+    } else if (nums1[p1] > nums2[p2]) {
+      cur = nums1[p1 --]
+    } else {
+      cur = nums2[p2 --]
+    }
+    nums1[len --] = cur
+  }
+};
+// @lc code=end
+
