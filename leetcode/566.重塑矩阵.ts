88317c397008acfeeced43aee7055750323ef3bc
cuiyiming1998 <1269567664@qq.com>
2022-08-22 11:22:00 +0800

    feat: leetcode 566

---
+++ leetcode/566.重塑矩阵.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=566 lang=typescript
+ *
+ * [566] 重塑矩阵
+ */
+
+// @lc code=start
+function matrixReshape(mat: number[][], r: number, c: number): number[][] {
+  // 思路: 映射
+  // (i, j) -> i * n + j
+  // i = Math.floor(x / n)
+  // j = x % n
+  // n 为列数 即 mat[0].length
+	let m: number = mat.length
+	let n: number = mat[0].length
+
+	if (m * n !== r * c) {
+		return mat
+	}
+
+	let res: number[][] = new Array(r).fill(0).map(() => new Array(c).fill(0))
+  for (let i = 0; i < m * n; ++ i) {
+    res[Math.floor(i / c)][i % c] = mat[Math.floor(i / n)][i % n]
+  }
+  return res
+}
+// @lc code=end
---
+++ leetcode/655.输出二叉树.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=655 lang=typescript
+ *
+ * [655] 输出二叉树
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// @ts-nocheck
+function printTree(root: TreeNode | null): string[][] {
+
+};
+// @lc code=end
+
