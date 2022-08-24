e468850c51b8e1152e0982c7ad28ab9d1d1f82e9
cuiyiming1998 <1269567664@qq.com>
2022-08-24 11:08:47 +0800

    feat: leetcode 387

---
+++ leetcode/1460.通过翻转子数组使两个数组相等.ts
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=1460 lang=typescript
+ *
+ * [1460] 通过翻转子数组使两个数组相等
+ */
+
+// @lc code=start
+function canBeEqual(target: number[], arr: number[]): boolean {
+  target.sort((a, b) => a - b)
+  arr.sort((a, b) => a - b)
+
+  return target.toString() === arr.toString()
+};
+// @lc code=end
+
---
+++ leetcode/387.字符串中的第一个唯一字符.ts
@@ -0,0 +1,32 @@
+/*
+ * @lc app=leetcode.cn id=387 lang=typescript
+ *
+ * [387] 字符串中的第一个唯一字符
+ */
+
+// @lc code=start
+function firstUniqChar(s: string): number {
+  // 思路: 使用map存储出现过的字符
+  // 如果没出现过 则更新value为下标
+  // 如果出现过则更新为false状态代表出现过
+  // 遍历map取第一个不为false的value即可
+  let map = new Map<string, boolean | number>()
+  let res: number = -1
+  for (let i = 0; i < s.length; i ++) {
+    if ('undefined' === typeof map.get(s[i])) {
+      map.set(s[i], i)
+    } else {
+      map.set(s[i], false)
+    }
+  }
+  for (let value of map.values()) {
+    if ('number' === typeof value) {
+      res = value
+      break
+    }
+  }
+
+  return res
+};
+// @lc code=end
+
