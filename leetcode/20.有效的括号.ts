490be0f30790500c218fc457b2292f0afabc1889
cuiyiming1998 <1269567664@qq.com>
2022-08-30 09:57:16 +0800

    feat: leetcode

---
+++ leetcode/20.有效的括号.ts
@@ -0,0 +1,34 @@
+/*
+ * @lc app=leetcode.cn id=20 lang=typescript
+ *
+ * [20] 有效的括号
+ */
+
+// @lc code=start
+function isValid(s: string): boolean {
+	const len = s.length
+	if (1 === len % 2) {
+		return false
+	}
+
+	const map = new Map<string, string>([
+		[')', '('],
+		[']', '['],
+		['}', '{']
+	])
+
+	const stack: string[] = []
+	for (const i of s) {
+		if (map.has(i)) {
+			if (!stack.length || stack[stack.length - 1] !== map.get(i)) {
+				return false
+			}
+			stack.pop()
+		} else {
+      stack.push(i)
+    }
+	}
+  return !stack.length;
+}
+
+// @lc code=end
---
+++ leetcode/83.删除排序链表中的重复元素.ts
@@ -0,0 +1,37 @@
+/*
+ * @lc app=leetcode.cn id=83 lang=typescript
+ *
+ * [83] 删除排序链表中的重复元素
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     val: number
+ *     next: ListNode | null
+ *     constructor(val?: number, next?: ListNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.next = (next===undefined ? null : next)
+ *     }
+ * }
+ */
+
+// @ts-nocheck
+function deleteDuplicates(head: ListNode | null): ListNode | null {
+  if (!head) {
+    return head
+  }
+
+  let cur = head
+  while (cur.next) {
+    if (cur.val === cur.next.val) {
+      cur.next = cur.next.next
+    } else {
+      cur = cur.next
+    }
+  }
+  return head
+};
+// @lc code=end
+
