3872e0fa43937d8f97402f23fe7494dc0e474289
cuiyiming1998 <1269567664@qq.com>
2022-10-22 20:33:57 +0800

    ✨ feat: 添加leetcode 102

---
+++ leetcode/0102.二叉树的层序遍历.ts
@@ -0,0 +1,50 @@
+/*
+ * @lc app=leetcode.cn id=102 lang=typescript
+ *
+ * [102] 二叉树的层序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for a binary tree node.
+ * class TreeNode {
+ *     val: number
+ *     left: TreeNode | null
+ *     right: TreeNode | null
+ *     constructor(val?: number, left?: TreeNode | null, right?: TreeNode | null) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.left = (left===undefined ? null : left)
+ *         this.right = (right===undefined ? null : right)
+ *     }
+ * }
+ */
+
+// @ts-nocheck
+function levelOrder(root: TreeNode | null): number[][] {
+  // 思路: 广度优先搜素
+  // 使用队列
+  const res: number[][] = []
+  if (!root) {
+    return res
+  }
+  let queue: TreeNode[] = []
+  queue.push(root)
+  while(queue.length !== 0) {
+    res.push([]) // 新的一层
+    const len = queue.length
+    for (let i = 0; i < len; i++) {
+      let node = queue.shift()
+      res[res.length - 1].push(node.val)
+      if (node.left) {
+        queue.push(node.left)
+      }
+      if (node.right) {
+        queue.push(node.right)
+      }
+    }
+  }
+
+  return res
+};
+// @lc code=end
+
