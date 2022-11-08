0add7f6078078b4ada18230613f995650d1ecdcb
cuiyiming1998 <1269567664@qq.com>
2022-11-09 00:01:25 +0800

    ✨ feat: leetcode 1684

---
+++ leetcode/1684.统计一致字符串的数目.ts
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=1684 lang=typescript
+ *
+ * [1684] 统计一致字符串的数目
+ */
+
+// @lc code=start
+function countConsistentStrings(allowed: string, words: string[]): number {
+  const set = new Set<string>()
+  for (const i of allowed) {
+    set.add(i)
+  }
+  let res: number = 0
+  for (const word of words) {
+    let matched:boolean = true
+
+    for (const c of word) {
+      if (!set.has(c)) {
+        matched = false
+        break
+      }
+    }
+    if (matched) {
+      res ++
+    }
+  }
+  return res
+};
+// @lc code=end
+
