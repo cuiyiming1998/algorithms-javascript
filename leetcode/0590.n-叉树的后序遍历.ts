0a9de95e9d5934f02b2a70c86bc815f3280f3681
cuiyiming1998 <1269567664@qq.com>
2022-11-28 11:37:59 +0800

    ✨ feat: 添加leetcode 590

---
+++ leetcode/0590.n-叉树的后序遍历.ts
@@ -0,0 +1,42 @@
+/*
+ * @lc app=leetcode.cn id=590 lang=typescript
+ *
+ * [590] N 叉树的后序遍历
+ */
+
+// @lc code=start
+/**
+ * Definition for node.
+ * class Node {
+ *     val: number
+ *     children: Node[]
+ *     constructor(val?: number) {
+ *         this.val = (val===undefined ? 0 : val)
+ *         this.children = []
+ *     }
+ * }
+ */
+
+// @ts-nocheck
+function postorder(root: Node | null): number[] {
+  let res: number[] = []
+
+  const order = (root: Node | null) => {
+    if (!root) {
+      return
+    }
+
+    if (root.children.length) {
+      root.children.map(node => {
+        order(node)
+      })
+    }
+    res.push(root.val)
+
+  }
+  order(root)
+
+  return res
+};
+// @lc code=end
+
