bd53c620b1dbdba6381522638615eab54ca1323f
cuiyiming1998 <1269567664@qq.com>
2022-11-29 11:01:46 +0800

    ✨ feat: leetcode 198

---
+++ leetcode/0198.打家劫舍.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=198 lang=typescript
+ *
+ * [198] 打家劫舍
+ */
+
+// @lc code=start
+function rob(nums: number[]): number {
+  // 思路: 动态规划
+  // dp[n] = Math.max(dp[n - 1], dp[n - 2] + num)
+
+  const len: number = nums.length
+  if (!len) {
+    return 0
+  }
+  let dp: number[] = new Array(len + 1)
+  dp[0] = 0
+  dp[1] = nums[0]
+
+  for (let i = 2; i < dp.length; i ++) {
+    dp[i] = Math.max(dp[i - 1], dp[i - 2] + nums[i - 1])
+  }
+
+  return dp[len]
+};
+// @lc code=end
+
