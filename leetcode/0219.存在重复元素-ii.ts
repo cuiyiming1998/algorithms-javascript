6bbbffba941f8fe6ba1c59f2ae505a191b841c6a
cuiyiming1998 <1269567664@qq.com>
2022-11-30 15:31:17 +0800

    ✨ feat: leetcode 219

---
+++ leetcode/0219.存在重复元素-ii.ts
@@ -0,0 +1,19 @@
+/*
+ * @lc app=leetcode.cn id=219 lang=typescript
+ *
+ * [219] 存在重复元素 II
+ */
+
+// @lc code=start
+function containsNearbyDuplicate(nums: number[], k: number): boolean {
+	const map = new Map<number, number>()
+	for (let i = 0; i < nums.length; i++) {
+		const curr: number = nums[i]
+		if (map.has(curr) && i - map.get(curr) <= k) {
+			return true
+		}
+		map.set(curr, i)
+	}
+	return false
+}
+// @lc code=end
