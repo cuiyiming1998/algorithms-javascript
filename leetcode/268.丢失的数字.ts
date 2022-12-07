a267cf8145a4163f2dc1d7c1f0dadd8f59dda063
cuiyiming1998 <1269567664@qq.com>
2022-12-07 11:27:39 +0800

    ✨ feat: leetcode 268

---
+++ leetcode/268.丢失的数字.ts
@@ -0,0 +1,27 @@
+/*
+ * @lc app=leetcode.cn id=268 lang=typescript
+ *
+ * [268] 丢失的数字
+ */
+
+// @lc code=start
+function missingNumber(nums: number[]): number {
+	// 思路: set
+
+	let set = new Set<number>()
+	const n: number = nums.length
+	for (let i = 0; i < n; i++) {
+		set.add(nums[i])
+	}
+
+	let res: number = -1
+	for (let i = 0; i <= n; i++) {
+		if (!set.has(i)) {
+			res = i
+			break
+		}
+	}
+
+	return res
+}
+// @lc code=end
