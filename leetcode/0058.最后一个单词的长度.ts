990a8686a7c2b08563395009f8b70de23247f4f5
cuiyiming1998 <1269567664@qq.com>
2022-12-30 16:25:34 +0800

    :sparkles: feat(leetcode): 添加58 最后一个单词的长度

---
+++ leetcode/0058.最后一个单词的长度.ts
@@ -0,0 +1,22 @@
+/*
+ * @lc app=leetcode.cn id=58 lang=typescript
+ *
+ * [58] 最后一个单词的长度
+ */
+
+// @lc code=start
+function lengthOfLastWord(s: string): number {
+  // 思路: 从后往前遍历
+  let index = s.length - 1
+  let len = 0
+  while (' ' === s[index]) {
+    index--
+  }
+  while (' ' !== s[index] && index >= 0) {
+    len++
+    index--
+  }
+
+  return len
+}
+// @lc code=end
