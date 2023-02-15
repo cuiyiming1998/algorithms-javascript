074a49a4b3f30f1d98a272a9869b6e65c5abae90
cuiyiming1998 <1269567664@qq.com>
2023-02-15 18:14:09 +0800

    :sparkles: feat(leetcode): add 0202

---
+++ leetcode/0202.快乐数.ts
@@ -0,0 +1,28 @@
+/*
+ * @lc app=leetcode.cn id=202 lang=typescript
+ *
+ * [202] 快乐数
+ */
+
+// @lc code=start
+function getNext(num: number) {
+  return num
+    .toString()
+    .split('')
+    .map((i) => Number(i) ** 2)
+    .reduce((a, b) => a + b)
+}
+function isHappy(n: number): boolean {
+  // 思路: 快慢指针
+  // 如果不是快乐数 最后会形成环 也就是快慢指针会相遇
+  let slow: number = n
+  let fast: number = getNext(n)
+
+  while (fast !== 1 && fast !== slow) {
+    slow = getNext(slow)
+    fast = getNext(getNext(fast))
+  }
+
+  return fast === 1
+}
+// @lc code=end
