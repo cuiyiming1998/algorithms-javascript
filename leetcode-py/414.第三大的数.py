11532e717504c03aed46b1c06d7689b208bb7089
cymYoung <1269567664@qq.com>
2025-12-19 14:20:37 +0800

    feat: leetcode 414(py)

---
+++ leetcode-py/414.第三大的数.py
@@ -0,0 +1,66 @@
+#
+# @lc app=leetcode.cn id=414 lang=python3
+#
+# [414] 第三大的数
+#
+# https://leetcode.cn/problems/third-maximum-number/description/
+#
+# algorithms
+# Easy (41.23%)
+# Likes:    498
+# Dislikes: 0
+# Total Accepted:    192.9K
+# Total Submissions: 468K
+# Testcase Example:  '[3,2,1]'
+#
+# 给你一个非空数组，返回此数组中 第三大的数 。如果不存在，则返回数组中最大的数。
+#
+#
+#
+# 示例 1：
+#
+#
+# 输入：[3, 2, 1]
+# 输出：1
+# 解释：第三大的数是 1 。
+#
+# 示例 2：
+#
+#
+# 输入：[1, 2]
+# 输出：2
+# 解释：第三大的数不存在, 所以返回最大的数 2 。
+#
+#
+# 示例 3：
+#
+#
+# 输入：[2, 2, 3, 1]
+# 输出：1
+# 解释：注意，要求返回第三大的数，是指在所有不同数字中排第三大的数。
+# 此例中存在两个值为 2 的数，它们都排第二。在所有不同数字中排第三大的数为 1 。
+#
+#
+#
+# 提示：
+#
+#
+# 1
+# -2^31
+#
+#
+#
+#
+# 进阶：你能设计一个时间复杂度 O(n) 的解决方案吗？
+#
+#
+from typing import List
+
+
+# @lc code=start
+class Solution:
+    def thirdMax(self, nums: List[int]) -> int:
+        return sorted(set(nums), reverse=True)[2] if len(set(nums)) >= 3 else max(nums)
+
+
+# @lc code=end
